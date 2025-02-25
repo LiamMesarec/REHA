43759a5ed27c140ca7247086ae5c5842594c160d
FikiFix <--filip.ferk@student.um.si>
2025-02-25 15:56:41 +0100

    file uploat started

--- app/app/fileUpload.tsx
+++ app/app/fileUpload.tsx
@@ -1,35 +1,33 @@
 import React, { useState } from 'react';
 import { View, Button, Alert, Text } from 'react-native';
-import DocumentPicker from 'react-native-document-picker';
+import * as DocumentPicker from 'expo-document-picker';
 
 const FileUploadScreen: React.FC = () => {
   const [file, setFile] = useState<any>(null);
 
-  const uploadToServer = async (file: any) => { //simulacija
+  // Simulated upload function
+  const uploadToServer = async (file: any) => {
     console.log('Simulating file upload...');
     setTimeout(() => {
       Alert.alert('Upload Successful', `File ${file.name} uploaded successfully!`);
     }, 2000);
   };
 
-  // Function to select a file
   const selectFile = async () => {
     try {
-      const res = await DocumentPicker.pick({
-        type: [DocumentPicker.types.allFiles], // You can specify different file types
+      const res = await DocumentPicker.getDocumentAsync({
+        type: '*/*',  // This will allow picking all types of documents
       });
 
-      // Store the file data
-      setFile(res[0]);
-
-      // Simulate file upload to an API
-      uploadToServer(res[0]);
-    } catch (err) {
-      if (DocumentPicker.isCancel(err)) {
+      if (res.type === 'cancel') {
         console.log('User canceled the picker');
-      } else {
-        console.error(err);
+        return;
       }
+
+      setFile(res);
+      uploadToServer(res);
+    } catch (err) {
+      console.error('Error picking document:', err);
     }
   };
 
@@ -42,6 +40,7 @@ const FileUploadScreen: React.FC = () => {
           <Text>Selected File:</Text>
           <Text>Name: {file.name}</Text>
           <Text>Size: {file.size} bytes</Text>
+          <Text>URI: {file.uri}</Text>
         </View>
       )}
     </View>
