da9498a3b41a95a01fe12a01b6191017c7d3dd73
MD20M <maj.donko@gmail.com>
2025-04-28 14:10:53 +0200

    Izboljšava login routa

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,3 +1,4 @@
+import { authHandler } from '../middleware/authHandler';
 const express = require('express');
 const router = express.Router();
 
@@ -9,41 +10,16 @@ const router = express.Router();
     "mail3",
 ]*/
 
-router.get('/', async (_req:any, res:any) => {
-    const authHeader = _req.headers.authorization;
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        //const decodedToken = jwt.decode(token, {issuer: 'https://login.microsoftonline.com/21bd0147-4d70-40b6-b482-8f63a0cb6e44/v2.0'});
-        //console.log(decodedToken.upn);
-        const response = await fetch('https://graph.microsoft.com/v1.0/me', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        if (response.status === 200) {
-            const resJson = await response.json();
-            const userList = await fetch('http://localhost:3000/api/users/list', {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const userListJson = await userList.json();
-            console.log(userListJson);
-            console.log(resJson.mail);
-            console.log(userListJson.some((user: any) => user.email === resJson.mail));
-            if (userListJson.some((user: any) => user.email === resJson.mail)) {
-                res.status(200).send({"message": "Authorized", "email": resJson.mail, "accessLevel": userListJson[resJson.mail]});
-            }
-            else{
-                res.status(401).send({"message": "Unauthorized"});
-            }
-        }
-        else{
-            res.status(401).send(await response.json().then((data:any) => {return data.error}));
-        }
-    } else {
-        res.status(401).send({"message": "Unauthorized"});
+router.get('/', authHandler, async (req:any, res:any) => {
+    const user = await req.body.user;
+    if (user == null) {
+        return res.status(401).send({ "message": "Unauthorized" });
     }
+    
+    if (user[1] < 3) {
+        return res.status(401).send({ "message": "Unauthorized" });
+    }
+
+    res.status(200).send({"message": "Authorized", "email": user[0], "accessLevel": user[1]});
 });
 export default router;
