033d40d3fce5057d26d79ce35037df9177fc2167
MarkoMilenovic <markomilenovic7@gmail.com>
2025-05-16 01:00:17 +0200

    file size fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,8 @@ import { authHandler } from './middleware/authHandler';
 export function App(db: Database): express.Application {
   const app = express();
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({ limit: '1024mb' }));
+  app.use(express.urlencoded({ limit: '1024mb', extended: true }));
   app.locals.db = db;
 
   app.use('/api/events', eventRoutes);
