90f311f39e03804007d63c1d0d3f15ae0b21429e
Liam Mesarec <mesarec.liam@gmail.com>
2025-05-15 13:01:32 +0200

    fixed vertical margin for maplist

--- app/app/mapList.tsx
+++ app/app/mapList.tsx
@@ -1,42 +1,57 @@
-// mapDisplay.tsx (Modified to accept props)
-import React, { useState, useEffect } from "react";
-import { Text, TouchableOpacity, View, StyleSheet, ScrollView } from "react-native";
+// mapDisplay.tsx
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Platform,
+} from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import { FileNode } from './types';
-import { Checkbox } from 'expo-checkbox';
+import { FileNode } from "./types";
+import { Checkbox } from "expo-checkbox";
 
 interface MapListProps {
   folders: FileNode[];
   onFolderPress: (folder: FileNode) => void;
-  selectedFolders : string[];
-  toggleSelectedFolder : (folderName : string) => void;
-  editVisible : boolean;
+  selectedFolders: string[];
+  toggleSelectedFolder: (folderName: string) => void;
+  editVisible: boolean;
 }
 
-const MapList: React.FC<MapListProps> = ({ folders, onFolderPress, selectedFolders, toggleSelectedFolder, editVisible }) => {
+const MapList: React.FC<MapListProps> = ({
+  folders,
+  onFolderPress,
+  selectedFolders,
+  toggleSelectedFolder,
+  editVisible,
+}) => {
+  const fileButtonMargin = Platform.OS === "web" ? 35 : 15;
+
   return (
     <View style={styles.container}>
-      {folders.map((folder, index) => (
-        <View key={folder.filePath} style={styles.fileContainer}>
-            {editVisible && <Checkbox
-            value={selectedFolders.includes(folder.filePath)}
-            onValueChange={() => toggleSelectedFolder(folder.filePath)}
-            />}
-          <TouchableOpacity
-            key={index}
-            style={styles.fileButton}
-            onPress={() => onFolderPress(folder)}
-          >
-            <View style={styles.rowContainer}>
-              <Icon name="folder" size={40} color="#F1C27D" />
-              <View style= {styles.mapNameContainer}>
-                <Text style={styles.text}>{folder.name}</Text>
-                <Text style={styles.dateText}>{folder.date}</Text>
-              </View>
-            </View>
-          </TouchableOpacity>
-        </View>
-      ))}
+    {folders.map((folder) => (
+      <View key={folder.filePath} style={styles.fileContainer}>
+      {editVisible && (
+        <Checkbox
+        value={selectedFolders.includes(folder.filePath)}
+        onValueChange={() => toggleSelectedFolder(folder.filePath)}
+        />
+      )}
+      <TouchableOpacity
+      style={[styles.fileButton, { marginVertical: fileButtonMargin }]}
+      onPress={() => onFolderPress(folder)}
+      >
+      <View style={styles.rowContainer}>
+      <Icon name="folder" size={40} color="#F1C27D" />
+      <View style={styles.mapNameContainer}>
+      <Text style={styles.text}>{folder.name}</Text>
+      <Text style={styles.dateText}>{folder.date}</Text>
+      </View>
+      </View>
+      </TouchableOpacity>
+      </View>
+    ))}
     </View>
   );
 };
@@ -52,13 +67,12 @@ const styles = StyleSheet.create({
   fileButton: {
     backgroundColor: "#ffffff",
     padding: 5,
-    marginVertical: 5,
     borderRadius: 5,
     borderColor: "black",
     borderWidth: 1,
     width: "100%",
-    alignItems: "flex-start", 
-    marginLeft : 5,
+    alignItems: "flex-start",
+    marginLeft: 5,
   },
   rowContainer: {
     flexDirection: "row",
@@ -70,20 +84,20 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginLeft: 10,
     textAlignVertical: "center",
-  },  
+  },
   dateText: {
     color: "black",
     fontSize: 10,
     fontWeight: "bold",
     marginLeft: 10,
     textAlignVertical: "center",
-  }, 
+  },
   fileContainer: {
-    flex : 1,
-    flexDirection : "row",
-    alignItems : "center"
-  },   
-  mapNameContainer : {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  mapNameContainer: {
     flex: 1,
     flexDirection: "column",
   },
