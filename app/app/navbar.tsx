be7c41253da9b3985f84a4232fb1452f07a25962
MD20M <maj.donko@gmail.com>
2025-05-22 15:59:29 +0200

    Better navbar

--- app/app/navbar.tsx
+++ app/app/navbar.tsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
-import { Image, Platform, Text, TouchableOpacity, View } from "react-native";
+import { Image, Platform, Text, TouchableOpacity, View, ScrollView } from "react-native";
 import { LoginButton } from "./login";
 import { AuthContext } from './authContext';
 import { StyleSheet } from 'react-native';
+import {wp, hp} from './size';
 
 import { useRouter } from "expo-router";
+import { SafeAreaView } from 'react-native-safe-area-context';
 export function Navbar() {
   const logoIcon = require('./logo_fakulteta.png');
   const logoLongIcon = require('./logo_fakulteta_long.png');
@@ -13,35 +15,39 @@ export function Navbar() {
   const router = useRouter();
 
   return (
-    <View style={styles.container}>
-    <View style={styles.navLeft}>
-    <TouchableOpacity style={styles.button} onPress={() => router.push('/calendar')}>
-    <Text style={styles.label}>Koledar</Text>
-    </TouchableOpacity>
+    <SafeAreaView style={[{ margin: 0}, Platform.OS== 'android' ? {marginBottom: -hp(5)} : {marginBottom: 0}]}>
+      <ScrollView horizontal={true}>
+        <View style={[styles.container, token ? { width: "100%" } : { width: wp(100) }]}>
+          <View style={styles.navLeft}>
+            <TouchableOpacity style={styles.button} onPress={() => router.push('/calendar')}>
+              <Text style={styles.label}>Koledar</Text>
+            </TouchableOpacity>
 
-    {token && (
-      <TouchableOpacity
-      style={styles.button}
-      onPress={() => router.push('/fileSystem')}
-      >
-      <Text style={styles.label}>E-Knjižnica</Text>
-      </TouchableOpacity>
-    )}
             {token && (
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => router.push('/whitelistDash')}
-          >
-            <Text style={styles.label}>Administracija</Text>
-          </TouchableOpacity>
-        )}
-    <TouchableOpacity style={styles.button} onPress={() => router.push('/about')}>
-    <Text style={styles.label}>O strani</Text>
-    </TouchableOpacity>
-    <LoginButton/>
-    </View>
-    <Image source={logoIcon} style={styles.logo} href="https://www.fzsv.si/" target="_blank" resizeMode="contain" />
-    </View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => router.push('/fileSystem')}
+              >
+                <Text style={styles.label}>E-Knjižnica</Text>
+              </TouchableOpacity>
+            )}
+            {token && (
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => router.push('/whitelistDash')}
+              >
+                <Text style={styles.label}>Administracija</Text>
+              </TouchableOpacity>
+            )}
+            <TouchableOpacity style={styles.button} onPress={() => router.push('/about')}>
+              <Text style={styles.label}>O strani</Text>
+            </TouchableOpacity>
+            <LoginButton />
+          </View>
+          <Image source={logoIcon} style={styles.logo} href="https://www.fzsv.si/" target="_blank" resizeMode="contain" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -54,6 +60,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     height: 64,
     elevation: 4,
+    marginBottom: 16,
   },
   navLeft: {
     flexDirection: 'row',
